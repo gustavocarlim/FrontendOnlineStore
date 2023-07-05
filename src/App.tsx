79493de771d729976f622cdd37bf1e9eadc74c8b
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-07-05 17:50:11 -0300

    requisito 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout';
+import { ShoppingCart } from './pages/ShoppingCart';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Layout /> } />
+      <Route path="/" element={ <Layout /> }>
+        <Route path="/shoppingcart" element={ <ShoppingCart /> } />
+      </Route>
     </Routes>
   );
 }
---
+++ src/pages/ShoppingCart/index.tsx
@@ -0,0 +1,5 @@
+export function ShoppingCart() {
+  return (
+    <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+  );
+}
