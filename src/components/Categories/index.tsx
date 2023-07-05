89ee950dccb2195a019d7ecff8e8a43fe4700c1c
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-07-05 19:24:51 -0300

    requisito 6

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from 'react';
-import { getCategories } from '../../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../../services/api';
+import CardsProduct from '../CardsProduct';
 
 export default function Categories() {
   const [categories, setCategories] = useState<{ id: string; name: string }[]>(
     [],
   );
+  const [products, setProducts] = useState([]);
 
+  const handleClick = async (Id: string) => {
+    if (Id) {
+      const productsData = await getProductsFromCategoryAndQuery(Id);
+      setProducts(productsData.results);
+    }
+  };
   useEffect(() => {
     async function fetchCategories() {
       const categoriesData = await getCategories();
@@ -26,6 +34,7 @@ export default function Categories() {
                   id={ `category-${category.id}` }
                   name="category"
                   value={ category.id }
+                  onClick={ () => handleClick(category.id) }
                 />
                 {category.name}
               </label>
@@ -33,6 +42,7 @@ export default function Categories() {
           ))}
         </ul>
       </nav>
+      <CardsProduct cards={ products } />
     </div>
   );
 }
