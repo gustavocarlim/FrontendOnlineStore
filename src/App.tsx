03e88becd8800335723f729e18098f7cfacb23b5
Victor Ciolac <victor.bcfraga@gmail.com>
2023-07-05 16:42:48 -0300

    Correção de lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React, { useEffect } from 'react';
-import { getCategories } from './services/api'; 
 import { Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout';
 
 function App() {
   return (
-   <Routes>
-      <Route path='/' element={<Layout />}>
-          
-      </Route>
-   </Routes>
-  )
+    <Routes>
+      <Route path="/" element={ <Layout /> } />
+    </Routes>
+  );
 }
 
 export default App;
